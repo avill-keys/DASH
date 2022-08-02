76608ff469d3eb8b674d8aa9992bd5b289bf05dc
Mircea Dan <mirceadan@gmail.com>
2022-08-02 10:25:00 -0700

    Update routingappliances.py

--- test/confgen/dashgen/routingappliances.py
+++ test/confgen/dashgen/routingappliances.py
@@ -3,35 +3,35 @@ from dashgen.variables import *
 
 def generate():
     print('    ' + os.path.basename(__file__))
-    routingappliances = []
+    routing_appliances = []
     for eni_index in range(1, ENI_COUNT+1):
-        j = eni_index+100
-        IP_L = IP_L_START + (eni_index - 1) * IP_STEP3
-        IP_R = IP_L_START + (j - 1) * IP_STEP3
-        routingappliances.append(
+        IP_L = IP_L_START + (eni_index - 1) * IP_STEP4
+        r_vpc = eni_index + ENI_L2R_STEP
+        IP_R = IP_R_START + (eni_index - 1) * IP_STEP4
+        routing_appliances.append(
             {
-                "VTEP:%d" % eni_index: {
-                    "appliance-id": "%d" % eni_index,
+                "ROUTING-APPLIANCE:%d" % eni_index: {
+                    "appliance-id": "appliance-%d" % eni_index,
                     "routing-appliance-id": eni_index,
                     "routing-appliance-addresses": [
-                        "%s/9" % IP_L
+                        "%s/32" % IP_L
                     ],
                     "encap-type": "vxlan",
                     "vni-key": eni_index
                 },
             }
         )
-        routingappliances.append(
+        routing_appliances.append(
             {
-                "VTEP:%d" % eni_index: {
-                    "appliance-id": "%d" % j,
-                    "routing-appliance-id": j,
+                "ROUTING-APPLIANCE:%d" % r_vpc: {
+                    "appliance-id": "appliance-%d" % r_vpc,
+                    "routing-appliance-id": r_vpc,
                     "routing-appliance-addresses": [
                         "%s/9" % IP_R
                     ],
                     "encap-type": "vxlan",
-                    "vni-key": j
+                    "vni-key": r_vpc
                 },
             }
         )
-    return {"routing-appliances": routingappliances}
+    return {"routing-appliances": routing_appliances}
